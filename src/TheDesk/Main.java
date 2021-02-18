c79d9159be1d809ade93ee87996bd67c4b7dfe90
mjtillman <mjtillman@gmail.com>
2021-02-18 12:58:00 -0600

    Made changes to project structure

--- src/TheDesk/Main.java
+++ src/TheDesk/Main.java
@@ -1,9 +1,5 @@
 package TheDesk;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +8,4 @@ public class Main {
         System.out.println("**************************************");
         DisplayMenus.optionsSelection();
     }
-
-
 }
